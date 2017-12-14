be32de93b9f6456306a2c016431fba4dbf14ded5
Charles Marnie Limpo <charlesmarnielimpo@gmail.com>
2017-12-14 08:11:02 +0800

    Test ftploy deployment

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'MainController@index');
+
+/**********************
+ * LOGIN AND SIGN UP  *
+ **********************/
+Auth::routes();
+// to logout user by entering /logout uri
+Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
+
+Route::get('/home', 'HomeController@index');
+
+Route::get('/test', 'HomeController@test');
+
+
+/******************
+ * ADMINISTRATOR  *
+ ******************/
+
+// Dashboard
+Route::get('/admin/dashboard', 'DashboardController@index')->middleware('admin')->name('dashboard');
+
+// Categories
+Route::get('/admin/categories', 'CategoryController@index')->middleware('admin')->name('categories');
