2f77ffe0a3927015a441756f7a1a940a6b37864d
Charles Marnie Limpo <charlesmarnielimpo@gmail.com>
2018-02-11 12:25:40 +0800

    Modified app.php

--- config/app.php
+++ config/app.php
@@ -162,11 +162,11 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        Gloudemans\Shoppingcart\ShoppingcartServiceProvider::class,
+        
         /*
          * Package Service Providers...
          */
-        Gloudemans\Shoppingcart\ShoppingcartServiceProvider::class,
         //
 
         /*
